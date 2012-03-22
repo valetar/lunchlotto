9d861b54b4b6587f7033cf46d93c7af310666a1e
Mirko Höch <mirko.hoech@edelight.de>
2012-03-22 16:25:46 +0100

    added marker handling; added failsafes for location getter

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
+var map;
+var initialLocation;
+var markers   = [];
+var siberia   = new google.maps.LatLng(60, 105);
+var stuttgart = new google.maps.LatLng(48.77065656676112, 9.179780947082463);
+var browserSupportFlag =  new Boolean();
+
 var lunchlotto = {
 	init: function() {
-		this.google.load();
 		// load jQuery from google cdn or fallback to local version
 		Modernizr.load([{
 		    load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
@@ -16,31 +22,84 @@ var lunchlotto = {
 		    	
 		    }
 		}]);
+		lunchlotto.google.maps();
 	},
 	google: {
-		load: function() {
-			Modernizr.load({
-			    load: 'http://maps.googleapis.com/maps/api/js?key=AIzaSyCA-4K9yCtNPaZ8CcEbx1V1WCcM5t-Wz40&sensor=false&callback=lunchlotto.google.maps'
-			});
-		},
 		maps: function() {
 			var myOptions = {
-	        	center: new google.maps.LatLng(48.777105, 9.180767999999999),
 	            zoom: 15,
 	            mapTypeId: google.maps.MapTypeId.ROADMAP
 	        };
 
-	        var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	        map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
-	        if (navigator.geolocation) {
-				// Use method getCurrentPosition to get coordinates
-				navigator.geolocation.getCurrentPosition(function(position) {
-					// Access them accordingly
-					lat = position.coords.latitude;
-					lon = position.coords.longitude;
-					$('#lat').val(lat);
-					$('#lon').val(lon);
-				});
+	        google.maps.event.addListener(map, 'click', function(event) {
+			    lunchlotto.google.setmarker(event.latLng);
+			});
+
+			// Try W3C Geolocation method (Preferred)
+  			if(navigator.geolocation) {
+    			browserSupportFlag = true;
+
+    			navigator.geolocation.getCurrentPosition(function(position) {
+	      			initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+	      			$('#lat').val(position.coords.latitude);
+					$('#lon').val(position.coords.longitude);
+
+					lunchlotto.google.setmarker(initialLocation);
+	    		}, function() {
+	      			hereandleNoGeolocation(browserSupportFlag);
+    			});
+  			} else if (google.gears) {
+			    // Try Google Gears Geolocation
+			    browserSupportFlag = true;
+
+			    var geo = google.gears.factory.create('beta.geolocation');
+			    geo.getCurrentPosition(function(position) {
+			      	initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+
+			      	$('#lat').val(position.latitude);
+					$('#lon').val(position.longitude);
+
+					lunchlotto.google.setmarker(initialLocation);
+			    }, function() {
+			     	handleNoGeolocation(browserSupportFlag);
+			    });
+			} else {
+			    // Browser doesn't support Geolocation
+			    browserSupportFlag = false;
+
+			    handleNoGeolocation(browserSupportFlag);
+			}
+		},
+		nolocation: function(errorFlag) {
+		  	if (errorFlag == true) {
+		    	initialLocation = stuttgart;
+		  	} else {
+		    	initialLocation = siberia;
+		  	}
+		  	lunchlotto.google.setmarker(initialLocation);
+		},
+		setmarker: function(location) {
+			lunchlotto.google.clearmarkers();
+
+			var marker = new google.maps.Marker({
+		    	position: location,
+		      	map: map,
+		      	title: 'You\'re here'
+		  	});
+			markers.push(marker);
+		  	map.setCenter(location);
+
+		  	$('#lat').val(location.Ua);
+			$('#lon').val(location.Va);
+		},
+		clearmarkers: function() {
+			if (markers) {
+			    for (i in markers) {
+      				markers[i].setMap(null);
+    			}
 			}
 		}
 	}
