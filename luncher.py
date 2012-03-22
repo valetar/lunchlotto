9e505a96708b934d5aa0b659dab7381b9d44991b
Alexander Valet <alexander.valet@edelight.de>
2012-03-22 17:19:55 +0100

    * Processing POST data * calling foursquare API

--- luncher.py
+++ luncher.py
@@ -1,11 +1,42 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
-def index(title='Lunchlotto - Where should I go for lunch today?'):
-    return render_template('base.html', title=title)
+from lib.foursquare import Foursquare, FoursquareAuth
+import settings
 
+client_id=settings.CLIENT_ID
+client_secret=settings.CLIENT_SECRET
+category_id=settings.CATEGORY_ID
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    auth=FoursquareAuth(client_id, client_secret)
+    foursq=Foursquare(auth)
+    venues=None
+    error=None
+    if request.method == 'POST':
+        lat=request.form.get('lat', '')
+        lon=request.form.get('lon', '')
+        radius=request.form.get('radius', 500)
+        cid=request.form.get('cid', category_id)
+        latlon='%s.%s,%s.%s' % (lat.split('.')[0],
+                                lat.split('.')[1][:2],
+                                lon.split('.')[0],
+                                lon.split('.')[1][:2],)
+        try:
+            res=foursq.request('venues', 
+                                aspect='search', 
+                                ll=latlon,
+                                radius=radius,
+                                categoryId=cid)
+            venues=res['response']['venues']
+        except Exception, e:
+            error='Connection error - %s ' % e
+    return render_template('base.html', venues=venues, error=error)
+                                              
 @app.route('/about')
 def about():
     return 'About lunchlotto'
@@ -15,4 +46,4 @@ def imprint():
     return 'Credits'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
